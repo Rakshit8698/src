6a9ca2ad44ae13671602699a7997375f564db1de
Rakshit Choudhary <krakshit8698@gmail.com>
2022-04-10 08:42:39 +0530

    Initial commit

---
+++ src/factors5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class factors5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i;
+        int a=sc.nextInt();
+        for (i =1;i*i<=a;i++){
+            if (a%i==0)
+                System.out.println(i);
+
+        }
+    }
+}
